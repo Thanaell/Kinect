287ca4ae29175797dd010435f6fd6f8bd1a727ec
Thanaell <gaelle_clavelin@orange.fr>
2018-10-13 20:06:07 +0200

    menu avec indicateur de connexion

---
+++ TestKinect/Assets/Script/DontDestroy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroy : MonoBehaviour {
+
+    private static bool created = false;
+
+    void Awake()
+    {
+        if (!created)
+        {
+            DontDestroyOnLoad(this.gameObject);
+            created = true;
+        }
+    }
+}
---
+++ TestKinect/Assets/Script/TestConnection.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestConnection : MonoBehaviour {
+    public Rigidbody objectTest;
+	// Use this for initialization
+	void Start () {
+        objectTest = GameObject.Find("TestConnection").GetComponent < Rigidbody >();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (objectTest.velocity == new Vector3(0,0,0))
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+        else
+        {
+            GetComponent<Renderer>().material.color = Color.green;
+        }
+	}
+}
