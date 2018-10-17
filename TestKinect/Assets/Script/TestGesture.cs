5479e14ff64806448a98773a8bd9338e29ef600f
Raminka <ramina.suleimanova@telecom-sudparis.eu>
2018-10-17 18:13:12 +0200

    gestes ameliorés : Right , Left, Closing et Running

--- TestKinect/Assets/Script/TestGesture.cs
+++ TestKinect/Assets/Script/TestGesture.cs
@@ -3,37 +3,86 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TestGesture : Gesture {
-    //nombre de frame
-    [SerializeField]
-    int FrameNumber = 5;
 
-    //nombre de points moyens prelevé pour considerer un geste 
-    [SerializeField]
-    int PointNumberSwippe = 4;
+
 
     //nombre de points prelevé pour considerer l'action SwippingLeft
     [SerializeField]
-    int PointNumberAction = 20;
+    int PointNumberAction = 50;
 
     [SerializeField]
-    float deltaX = 0.01f;
+    float deltaX = 0.1f;
 
     [SerializeField]
-    float deltaY = 0.01f;
+    float deltaY = 0.05f;
+
+    [SerializeField]
+    float distanceMaxX = 0.1f;
 
 
     public GameObject epauleDroite;
-    public GameObject coudeDroite;
+    public GameObject coudeDroit;
     public GameObject mainDroite;
+
+    bool FirstPosition;
+    int compteurLeftRight ;
+    bool SecondPosition;
+    int compteurRightLeft;
 
     public override void localStart()
     {
-
+        FirstPosition = false;
+        compteurLeftRight = 0;
+        SecondPosition = false;
+        compteurRightLeft = 0;
     }
 
     public override void detect()
     {
 
+        if (Mathf.Abs(epauleDroite.transform.position.x - mainDroite.transform.position.x) < deltaX )
+        {
+            if (SecondPosition)
+            {
+                isDetected = true;
+                SecondPosition = false;
+                compteurRightLeft = 0;
+            }
+            FirstPosition = true;
+            compteurLeftRight = PointNumberAction/2;
+
+        }
+        if (compteurLeftRight> 0)
+        {
+            compteurLeftRight--;
+        }
+        else
+        {
+            compteurLeftRight = 0;
+            FirstPosition = false;
+        }
+
+        if (Mathf.Abs(coudeDroit.transform.position.y - mainDroite.transform.position.y) < deltaY &
+            Mathf.Abs(mainDroite.transform.position.y - coudeDroit.transform.position.y) < deltaY &
+            epauleDroite.transform.position.x-mainDroite.transform.position.x>distanceMaxX &
+            FirstPosition)
+        {
+            FirstPosition = false;
+            compteurLeftRight = 0;
+            compteurRightLeft = PointNumberAction / 2;
+            SecondPosition = true;
+        }
+        if (compteurRightLeft > 0)
+        {
+            compteurRightLeft--;
+        }
+        else
+        {
+            compteurRightLeft = 0;
+            SecondPosition = false;
+        }
+
     }
+
 
 }
