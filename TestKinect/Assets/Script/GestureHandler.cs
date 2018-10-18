632335103fbae691e21f808dff89873354efd2c8
Thanaell <gaelle_clavelin@orange.fr>
2018-10-18 17:45:28 +0200

    affichage des gestes à l'écran

--- TestKinect/Assets/Script/GestureHandler.cs
+++ TestKinect/Assets/Script/GestureHandler.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GestureHandler : MonoBehaviour {
-
+    [SerializeField]
+    private Text text;
 	// Use this for initialization
 	void Start () {
-		
+        text.text = "No Gesture";
 	}
 	
 	// Update is called once per frame
@@ -16,6 +18,7 @@ public class GestureHandler : MonoBehaviour {
 
     public void GestureDisplay(System.Type type)
     {
+        text.text = type.ToString();
         Debug.Log(type);
     }
 }
